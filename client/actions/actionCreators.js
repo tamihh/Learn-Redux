86870f0486e6dcddb53a592abeb49ffbdf1df069
tamih <tamiris.simoneli@gmail.com>
2016-11-16 01:15:41 -0200

    created store and actions

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,26 @@
+//incrment
+export function increment(index) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    }
+}
+
+// add comment
+export function addComent(postId, author, comment) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+}
+
+// remove comment
+export function removeComment(postId, i) {
+    return {
+        type: 'REMOVE_COMMENT',
+        i,
+        postId
+    }
+}
