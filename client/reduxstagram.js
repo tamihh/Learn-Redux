9f6432674fa4fbee160500eae41c6d4cf510b348
tamih <tamiris.simoneli@gmail.com>
2016-11-15 23:46:26 -0200

    setup projects

---
+++ client/reduxstagram.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+// import css
+import css from './styles/style.styl';
+
+//import components
+import Main from './components/Main';
+
+render(<Main/>, document.getElementById('root'));
